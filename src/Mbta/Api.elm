d4f0598edbe1e688f016ec8c588530fa094c0ad2
Sky Rose <sky.q.rose@gmail.com>
2019-04-29 19:32:26 -0400

    compiles

--- src/Mbta/Api.elm
+++ src/Mbta/Api.elm
@@ -1,10 +1,8 @@
-module Mbta.Api exposing (ApiKey(..), Config, Host(..), getStops)
-
-import Http
-import Json.Decode as Decode
-import Mbta as Mbta
-import Mbta.Decoders
-import Mbta.Url
+module Mbta.Api exposing
+    ( ApiKey(..)
+    , Config
+    , Host(..)
+    )
 
 
 type Host
@@ -13,32 +11,11 @@ type Host
 
 
 type ApiKey
-    = ApiKey String
+    = NoApiKey
+    | ApiKey String
 
 
 type alias Config =
     { host : Host
     , apiKey : ApiKey
     }
-
-
-getStops : (Result Http.Error (List Mbta.Stop) -> msg) -> List Mbta.StopId -> Cmd msg
-getStops msg stopIds =
-    let
-        stopIdsParam =
-            stopIds
-                |> List.map (\(Mbta.StopId stopId) -> stopId)
-                |> String.join ","
-
-        url =
-            Mbta.Url.url "stops" [ ( "filter[id]", stopIdsParam ) ]
-    in
-    Http.get
-        { url = url
-        , expect =
-            Http.expectJson msg
-                (Mbta.Decoders.stopDecoder
-                    |> Decode.list
-                    |> Decode.at [ "data" ]
-                )
-        }
