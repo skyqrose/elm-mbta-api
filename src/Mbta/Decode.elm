0df273c6fd3166d111814e65d130d34c2d022958
Sky Rose <sky.q.rose@gmail.com>
2019-05-04 21:18:12 -0400

    expose JsonApi decode pipeline functions

--- src/Mbta/Decode.elm
+++ src/Mbta/Decode.elm
@@ -6,16 +6,23 @@ module Mbta.Decode exposing
 import DecodeHelpers
 import Dict
 import Json.Decode as Decode exposing (Decoder)
-import Json.Decode.Pipeline as Pipeline
 import JsonApi
+    exposing
+        ( attribute
+        , custom
+        , id
+        , relationshipMany
+        , relationshipMaybe
+        , relationshipOne
+        )
 import Mbta exposing (..)
 
 
 latLng : JsonApi.Resource -> Decoder LatLng
 latLng =
     JsonApi.decode LatLng
-        |> JsonApi.attribute "latitude" Decode.float
-        |> JsonApi.attribute "longitude" Decode.float
+        |> attribute "latitude" Decode.float
+        |> attribute "longitude" Decode.float
 
 
 wheelchairAccessible : Decoder WheelchairAccessible
@@ -35,16 +42,16 @@ stopId =
 stop : JsonApi.Resource -> Decoder Stop
 stop =
     JsonApi.decode Stop
-        |> JsonApi.id stopId
-        |> JsonApi.attribute "name" Decode.string
-        |> JsonApi.attribute "description" (Decode.nullable Decode.string)
-        |> JsonApi.relationshipMaybe "parent_station" stopId
-        |> JsonApi.attribute "platform_code" (Decode.nullable Decode.string)
-        |> JsonApi.attribute "platform_name" (Decode.nullable Decode.string)
-        |> JsonApi.attribute "location_type" locationType
-        |> JsonApi.custom latLng
-        |> JsonApi.attribute "address" (Decode.nullable Decode.string)
-        |> JsonApi.attribute "wheelchair_boarding" wheelchairAccessible
+        |> id stopId
+        |> attribute "name" Decode.string
+        |> attribute "description" (Decode.nullable Decode.string)
+        |> relationshipMaybe "parent_station" stopId
+        |> attribute "platform_code" (Decode.nullable Decode.string)
+        |> attribute "platform_name" (Decode.nullable Decode.string)
+        |> attribute "location_type" locationType
+        |> custom latLng
+        |> attribute "address" (Decode.nullable Decode.string)
+        |> attribute "wheelchair_boarding" wheelchairAccessible
 
 
 locationType : Decoder LocationType
