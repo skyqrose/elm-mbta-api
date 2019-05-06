c81b5d6322e66ad4a007926612dbace7a1da126c
Sky Rose <sky.q.rose@gmail.com>
2019-05-05 21:44:29 -0400

    api gets

--- src/Mbta/Api.elm
+++ src/Mbta/Api.elm
@@ -2,6 +2,22 @@ module Mbta.Api exposing
     ( ApiKey(..)
     , Config
     , Host(..)
+    , getPredictions
+    , getRoute
+    , getRoutePattern
+    , getRoutePatterns
+    , getRoutes
+    , getSchedules
+    , getService
+    , getServices
+    , getShape
+    , getShapes
+    , getStop
+    , getStops
+    , getTrip
+    , getTrips
+    , getVehicle
+    , getVehicles
     )
 
 import DecodeHelpers
@@ -72,14 +88,108 @@ getCustomList toMsg config resourceDecoder path =
 
 
 
--- /stop
+-- predictions+schedules
+
+
+getPredictions : (Result Http.Error (List Prediction) -> msg) -> Config -> Cmd msg
+getPredictions toMsg config =
+    getCustomList toMsg config Mbta.Decode.prediction "predictions"
+
+
+getSchedules : (Result Http.Error (List Schedule) -> msg) -> Config -> Cmd msg
+getSchedules toMsg config =
+    getCustomList toMsg config Mbta.Decode.schedule "schedules"
+
+
+
+-- routes
+
+
+getRoute : (Result Http.Error Route -> msg) -> Config -> RouteId -> Cmd msg
+getRoute toMsg config (RouteId routeId) =
+    getCustomId toMsg config Mbta.Decode.route "routes" routeId
+
+
+getRoutes : (Result Http.Error (List Route) -> msg) -> Config -> Cmd msg
+getRoutes toMsg config =
+    getCustomList toMsg config Mbta.Decode.route "routes"
+
+
+getRoutePattern : (Result Http.Error RoutePattern -> msg) -> Config -> RoutePatternId -> Cmd msg
+getRoutePattern toMsg config (RoutePatternId routePatternId) =
+    getCustomId toMsg config Mbta.Decode.routePattern "route-patterns" routePatternId
+
+
+getRoutePatterns : (Result Http.Error (List RoutePattern) -> msg) -> Config -> Cmd msg
+getRoutePatterns toMsg config =
+    getCustomList toMsg config Mbta.Decode.routePattern "route-patterns"
+
+
+
+-- services
+
+
+getService : (Result Http.Error Service -> msg) -> Config -> ServiceId -> Cmd msg
+getService toMsg config (ServiceId serviceId) =
+    getCustomId toMsg config Mbta.Decode.service "services" serviceId
+
+
+getServices : (Result Http.Error (List Service) -> msg) -> Config -> Cmd msg
+getServices toMsg config =
+    getCustomList toMsg config Mbta.Decode.service "services"
+
+
+
+-- shapes
+
+
+getShape : (Result Http.Error Shape -> msg) -> Config -> ShapeId -> Cmd msg
+getShape toMsg config (ShapeId shapeId) =
+    getCustomId toMsg config Mbta.Decode.shape "shapes" shapeId
+
+
+getShapes : (Result Http.Error (List Shape) -> msg) -> Config -> Cmd msg
+getShapes toMsg config =
+    getCustomList toMsg config Mbta.Decode.shape "shapes"
+
+
+
+-- stops
 
 
 getStop : (Result Http.Error Stop -> msg) -> Config -> StopId -> Cmd msg
 getStop toMsg config (StopId stopId) =
-    getCustomId toMsg config Mbta.Decode.stop "stop" stopId
+    getCustomId toMsg config Mbta.Decode.stop "stops" stopId
 
 
 getStops : (Result Http.Error (List Stop) -> msg) -> Config -> Cmd msg
 getStops toMsg config =
-    getCustomList toMsg config Mbta.Decode.stop "stop"
+    getCustomList toMsg config Mbta.Decode.stop "stops"
+
+
+
+-- trips
+
+
+getTrip : (Result Http.Error Trip -> msg) -> Config -> TripId -> Cmd msg
+getTrip toMsg config (TripId tripId) =
+    getCustomId toMsg config Mbta.Decode.trip "trips" tripId
+
+
+getTrips : (Result Http.Error (List Trip) -> msg) -> Config -> Cmd msg
+getTrips toMsg config =
+    getCustomList toMsg config Mbta.Decode.trip "trips"
+
+
+
+-- vehicles
+
+
+getVehicle : (Result Http.Error Vehicle -> msg) -> Config -> VehicleId -> Cmd msg
+getVehicle toMsg config (VehicleId vehicleId) =
+    getCustomId toMsg config Mbta.Decode.vehicle "vehicles" vehicleId
+
+
+getVehicles : (Result Http.Error (List Vehicle) -> msg) -> Config -> Cmd msg
+getVehicles toMsg config =
+    getCustomList toMsg config Mbta.Decode.vehicle "vehicles"
