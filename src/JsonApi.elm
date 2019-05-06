e5f1182650b77c49aa61688c1991eaea4a2de9e9
Sky Rose <sky.q.rose@gmail.com>
2019-05-05 20:24:00 -0400

    JsonApi docs

--- src/JsonApi.elm
+++ src/JsonApi.elm
@@ -1,19 +1,10 @@
 module JsonApi exposing
-    ( Decoder
-    , IdDecoder
-    , attribute
-    , custom
-    , decode
-    , decoderMany
-    , decoderOne
-    , id
-    , idDecoder
-    , relationshipMany
-    , relationshipMaybe
-    , relationshipOne
+    ( Decoder, IdDecoder, idDecoder
+    , decode, id, attribute, relationshipOne, relationshipMaybe, relationshipMany, custom
+    , decoderOne, decoderMany
     )
 
-{-| This module serves as a middle point between the raw json in JSON API
+{-| This module serves as a middle point between the raw JSON in JSON:API
 and the Elm types for the data you get out of it.
 
     type BookId
@@ -34,12 +25,27 @@ and the Elm types for the data you get out of it.
         decode resource
             |> id bookIdDecoder
             |> relationshipOne "author" authorIdDecoder
-            |> attribute "title" Decode.string
+            |> attribute "title" Json.Decode.string
 
     Http.get
         { url = url
         , expect = expectJson toMsg (decoderOne bookDecoder)
         }
+
+@docs Decoder, IdDecoder, idDecoder
+
+
+# Pipelines
+
+You can make `Decoder`s using a pipeline, modeled off of [`NoRedInk/elm-json-decode-pipeline`](Pipeline)
+[Pipeline][https://package.elm-lang.org/packages/NoRedInk/elm-json-decode-pipeline/latest/Json-Decode-Pipeline]
+
+@docs decode, id, attribute, relationshipOne, relationshipMaybe, relationshipMany, custom
+
+
+# Running a Decoder
+
+@docs decoderOne, decoderMany
 
 -}
 
@@ -111,19 +117,59 @@ resourceIdDecoder =
 
 
 
--- Pipeline for decoding internal resources into more specific types
+-- Public
 
 
-{-| -}
+{-| A `Decoder` knows how to turn untyped JSON:API data into usable Elm data.
+
+Note that this is a different type than `Json.Decode.Decoder`.
+A `JsonApi.Decoder` is specifically for working with JSON in the JSON:API format.
+It does not know how to work on general JSON.
+
+To turn this into a more general `Json.Decode.Decoder` and run it,
+see [`decoderOne`](#decoderOne) or [`decoderMany`](#decoderMany)
+
+-}
 type alias Decoder a =
     Resource -> Decode.Decoder a
 
 
+{-| JSON:API represents ids as
+
+    {
+        "type": "book",
+        "id": "0-06-443017-0"
+    }
+
+which works fine for dynamically typed languages,
+but doesn't work well with Elm's type system.
+An `IdDecoder` know how to convert those ids into typesafe Elm objects
+
+Create an `IdDecoder` with [`idDecoder`](#idDecoder)
+
+-}
 type alias IdDecoder a =
     ResourceId -> Decode.Decoder a
 
 
-{-| -}
+{-| Create an [`IdDecoder`](#IdDecoder)
+
+    type BookId
+        = BookId String
+
+    bookIdDecoder : IdDecoder BookId
+    bookIdDecoder =
+        idDecoder "book" BookId
+
+The `String` provided will be checked against the JSON:API's `type` field.
+If it doesn't match, decoding will fail.
+This provides protection that one id type can not be accidentally confused with another.
+
+It's recommended that you wrap id types with a custom type with one variant, like `BookId` above.
+This lets the Elm Compiler check that different id types don't get mixed up within your Elm code.
+`idDecoder` needs that constructor.
+
+-}
 idDecoder : String -> (String -> id) -> IdDecoder id
 idDecoder typeString idConstructor resourceId =
     if resourceId.resourceType == typeString then
@@ -142,11 +188,35 @@ idDecoder typeString idConstructor resourceId =
             )
 
 
+
+-- Pipeline
+
+
+{-| Start a decoding pipeline.
+
+Note that while [`NoRedInk/elm-json-decode-pipeline`](Pipeline) starts its pipelines with `Json.Decode.succeed`,
+the different `Decoder` type in this module needs to start its pipelines slightly differently
+
+    bookDecoder : Decoder Book
+    bookDecoder resource =
+        decode resource
+            |> id bookIdDecoder
+            |> attribute "title" Decode.string
+
+[Pipeline](https://package.elm-lang.org/packages/NoRedInk/elm-json-decode-pipeline/latest/Json-Decode-Pipeline)
+
+-}
 decode : constructor -> Decoder constructor
 decode constructor =
     \resource -> Decode.succeed constructor
 
 
+{-| Use an id within a pipeline
+
+This is for the id of the resource being decoded.
+For the id of a related resource, use one of the `relationship` functions below.
+
+-}
 id : IdDecoder id -> Decoder (id -> rest) -> Decoder rest
 id idDecoder_ =
     custom
@@ -155,6 +225,35 @@ id idDecoder_ =
         )
 
 
+{-| Use a resource's attribute within a pipeline
+
+Since attributes can be arbitrary JSON, this function takes a general use `Json.Decode.Decoder`.
+
+-}
+attribute : String -> Decode.Decoder attribute -> Decoder (attribute -> rest) -> Decoder rest
+attribute attributeName attributeDecoder =
+    custom
+        (\resource ->
+            case Dict.get attributeName resource.attributes of
+                Just attributeJson ->
+                    case Decode.decodeValue attributeDecoder attributeJson of
+                        Ok attribute_ ->
+                            Decode.succeed attribute_
+
+                        Err error ->
+                            Decode.fail (Decode.errorToString error)
+
+                Nothing ->
+                    Decode.fail ("Expected attribute " ++ attributeName ++ ", but it's missing")
+        )
+
+
+{-| Use a to-one relationship that must exist in a pipeline.
+
+This will add an id to the result.
+It will not look up the object in the `includes` and inline it.
+
+-}
 relationshipOne : String -> IdDecoder relatedId -> Decoder (relatedId -> rest) -> Decoder rest
 relationshipOne relationshipName relatedIdDecoder =
     custom
@@ -168,6 +267,12 @@ relationshipOne relationshipName relatedIdDecoder =
         )
 
 
+{-| Use a to-one relationship that might not exist in a pipeline
+
+This is for a relationship whose value is `null`.
+A relationship that that does not appear in the `"relationships"` object will fail to decode.
+
+-}
 relationshipMaybe : String -> IdDecoder relatedId -> Decoder (Maybe relatedId -> rest) -> Decoder rest
 relationshipMaybe relationshipName relatedIdDecoder =
     custom
@@ -194,6 +299,8 @@ relationshipMaybe relationshipName relatedIdDecoder =
         )
 
 
+{-| Use a to-many relationship in a pipeline
+-}
 relationshipMany : String -> IdDecoder relatedId -> Decoder (List relatedId -> rest) -> Decoder rest
 relationshipMany relationshipName relatedIdDecoder =
     let
@@ -230,24 +337,11 @@ relationshipMany relationshipName relatedIdDecoder =
         )
 
 
-attribute : String -> Decode.Decoder attribute -> Decoder (attribute -> rest) -> Decoder rest
-attribute attributeName attributeDecoder =
-    custom
-        (\resource ->
-            case Dict.get attributeName resource.attributes of
-                Just attributeJson ->
-                    case Decode.decodeValue attributeDecoder attributeJson of
-                        Ok attribute_ ->
-                            Decode.succeed attribute_
+{-| Run an arbitrary [`Decoder`](#Decoder) and include its result in the pipeline
 
-                        Err error ->
-                            Decode.fail (Decode.errorToString error)
+Useful for decoding complex objects
 
-                Nothing ->
-                    Decode.fail ("Expected attribute " ++ attributeName ++ ", but it's missing")
-        )
-
-
+-}
 custom : Decoder a -> Decoder (a -> rest) -> Decoder rest
 custom decoder constructorDecoder =
     \resource ->
@@ -261,6 +355,8 @@ custom decoder constructorDecoder =
 -- Run it
 
 
+{-| Create a `Json.Decode.Decoder` that can decode a JSON:API document representing a single resource
+-}
 decoderOne : Decoder a -> Decode.Decoder a
 decoderOne decoder =
     resourceDecoder
@@ -268,6 +364,8 @@ decoderOne decoder =
         |> Decode.andThen decoder
 
 
+{-| Create a `Json.Decode.Decoder` that can decode a JSON:API document representing many resources
+-}
 decoderMany : Decoder a -> Decode.Decoder (List a)
 decoderMany decoder =
     resourceDecoder
